7d78811df51de3cd8ac9c7753ef0ae1d77ba9368
Mendieta-Wendy23 <shinobuespindola@gmail.com>
2026-05-27 16:54:10 -0600

    muestra los datos del alumno

---
+++ diccionario.py
@@ -0,0 +1,18 @@
+# Creamos un diccionario llamado alumno
+alumno = {
+
+    "nombre": "Wendy",  # Guardamos el nombre del alumno
+
+    "carrera": "Programacion",  # Guardamos la carrera
+
+    "semestre": 2  # Guardamos el semestre
+}
+
+# Mostramos el nombre guardado en el diccionario
+print("Nombre:", alumno["nombre"])
+
+# Mostramos la carrera guardada en el diccionario
+print("Carrera:", alumno["carrera"])
+
+# Mostramos el semestre guardado en el diccionario
+print("Semestre:", alumno["semestre"])
